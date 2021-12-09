4bfc9cb7f97d0e91c056c9f6150439275d00368f
Xopek <nakit21@gmail.com>
2021-12-09 21:23:28 +0200

    add linters

--- scripts/merkletree.js
+++ scripts/merkletree.js
@@ -1,13 +1,13 @@
-const { MerkleTree } = require("merkletreejs");
-const keccak256 = require("keccak256")
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
 
-let whitelistAddress = [
-    "0x9e6a2A5Ac4D55eE0952aC3c09e6144353DD3d8DE"
-]
+const whitelistAddress = [
+  '0x9e6a2A5Ac4D55eE0952aC3c09e6144353DD3d8DE',
+];
 
-const leafNodes = whitelistAddress.map(addr => keccak256(addr));
-const merkleTree = new MerkleTree(leafNodes, keccak256, {sortPairs: true});
+const leafNodes = whitelistAddress.map((addr) => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 const rootHash = merkleTree.getRoot();
 
-console.log("Whitelist Merkle Tree\n", merkleTree.toString());
+console.log('Whitelist Merkle Tree\n', merkleTree.toString());
 console.log(rootHash.toString('hex'));
